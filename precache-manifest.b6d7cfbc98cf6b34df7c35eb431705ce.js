f765e4e2a65e7277d1f0698d5754852109f3d153
elenaortega <elenaorfe@gmail.com>
2019-04-13 17:42:15 +0200

    Updates

--- precache-manifest.b6d7cfbc98cf6b34df7c35eb431705ce.js
+++ precache-manifest.b6d7cfbc98cf6b34df7c35eb431705ce.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
   },
   {
-    "revision": "77df3854a8a91b80b5b4",
-    "url": "/portfolio/static/js/main.d05bfae2.chunk.js"
+    "revision": "d9b5614169f0375044db",
+    "url": "/portfolio/static/js/main.d3f0a9a5.chunk.js"
   },
   {
     "revision": "9eee67cf782a38d35aeb",
     "url": "/portfolio/static/js/2.d6a534e5.chunk.js"
   },
   {
-    "revision": "77df3854a8a91b80b5b4",
-    "url": "/portfolio/static/css/main.f4a33d04.chunk.css"
+    "revision": "d9b5614169f0375044db",
+    "url": "/portfolio/static/css/main.a857ac4e.chunk.css"
   },
   {
-    "revision": "ad5e970f810409e6c0cf281277eb2d5e",
+    "revision": "2cc32fe80f1c4cddf0b55bfe02849973",
     "url": "/portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.cc35643401723216574f13dbe046bec4.js"
+  "/portfolio/precache-manifest.b6d7cfbc98cf6b34df7c35eb431705ce.js"
 );
 
 workbox.clientsClaim();
