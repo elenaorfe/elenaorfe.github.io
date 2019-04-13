b4e149551882556b78c8f454b4b26e440f12b3ae
elenaortega <elenaorfe@gmail.com>
2019-04-13 19:04:05 +0200

    Add icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import icono from '../assets/Icono.png';
 
 class Header extends Component {
     render() {
         return (
             <header className='header'>
                 <div className='container'>
-                    <h1>Elena Ortega</h1>
-                    <h2>Frontend Developer</h2>
+                    <div className="row">
+                        <div className="col-4">
+                            <h1>Elena Ortega</h1>
+                            <h2>Frontend Developer</h2>
+                        </div>
+                        <div className="col-8">
+                            <img src={icono} />
+                        </div>
+                    </div>
                 </div>
             </header>
         );
