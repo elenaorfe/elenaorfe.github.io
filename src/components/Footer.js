9e96afa51066febc8c2ec1e24f3899a168cddf3d
elenaortega <elenaorfe@gmail.com>
2019-04-13 20:07:21 +0200

    Footer - Remove Contact text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,23 +4,16 @@ class Footer extends Component {
     render() {
         return (
             <footer className='footer'>
-                <div className='container'>
-                    <div className='row'>
-                        <div className='col-4'>
-                            <h2>Contact</h2>
-                        </div>
-                        <div className='col-8'>
-                            <a href='mailto:elenaorfe@gmail.com'>
-                                <i className='far fa-envelope' />
-                            </a>
-                            <a href='https://linkedin.com/in/elenaorfe' className='not-light' target='_blank'>
-                                <i className='fab fa-linkedin-in' />
-                            </a>
-                            <a href='https://github.com/elenaorfe' className='not-light' target='_blank'>
-                                <i className='fab fa-github' />
-                            </a>
-                        </div>
-                    </div>
+                <div className='container text-center'>
+                    <a href='mailto:elenaorfe@gmail.com'>
+                        <i className='far fa-envelope' />
+                    </a>
+                    <a href='https://linkedin.com/in/elenaorfe' className='not-light' target='_blank'>
+                        <i className='fab fa-linkedin-in' />
+                    </a>
+                    <a href='https://github.com/elenaorfe' className='not-light' target='_blank'>
+                        <i className='fab fa-github' />
+                    </a>
                 </div>
             </footer>
         );
