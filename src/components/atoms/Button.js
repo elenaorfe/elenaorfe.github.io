eb75975513b4513284a95196e9f3bba6c63ec2e5
elenaortega <elenaorfe@gmail.com>
2019-05-17 21:07:38 +0200

    Add CV download multilanguage

--- src/components/atoms/Button.js
+++ src/components/atoms/Button.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import CV from '../../assets/CV_ElenaOrtega.pdf'
 
 class Button extends Component {
     render() {
         return (
-            <a className={`btn ${this.props.type}`} href={CV} download>{this.props.text}</a>
+            <a className={`btn ${this.props.type}`} href={this.props.file} download>{this.props.text}</a>
         );
     }
 }
