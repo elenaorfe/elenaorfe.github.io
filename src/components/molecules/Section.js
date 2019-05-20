080dc60e596d3d89ddab07566083b94cbd21f7ba
elenaortega <elenaorfe@gmail.com>
2019-05-20 17:45:14 +0200

    Adjust responsive menu

--- src/components/molecules/Section.js
+++ src/components/molecules/Section.js
@@ -3,7 +3,6 @@ import Card from '../atoms/Card';
 
 class Section extends Component {
     renderCard() {
-        console.log(this.props.data)
         return this.props.data.map(item => {
             return <Card data={item} type={this.props.type}/>
         })
