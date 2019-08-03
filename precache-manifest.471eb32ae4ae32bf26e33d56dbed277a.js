f292d98f662509692d250a002e26d2e72874d9bd
elenaortega <elenaorfe@gmail.com>
2019-08-03 15:51:23 +0200

    Updates

---
+++ precache-manifest.471eb32ae4ae32bf26e33d56dbed277a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c260e949213f8de78339",
+    "url": "/portfolio/static/css/main.0752b405.chunk.css"
+  },
+  {
+    "revision": "c260e949213f8de78339",
+    "url": "/portfolio/static/js/main.fcc5c688.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "8839670083ee3533f9f2",
+    "url": "/portfolio/static/js/2.2947acce.chunk.js"
+  },
+  {
+    "revision": "df67fce8d912ed3ae937505b47a607f4",
+    "url": "/portfolio/static/media/profile.df67fce8.png"
+  },
+  {
+    "revision": "7f869362d9766f39ebcb40bf9d8afd8a",
+    "url": "/portfolio/static/media/CV_ElenaOrtegaFernandez_EN.7f869362.pdf"
+  },
+  {
+    "revision": "fe9e5e32702a5fa14d8aed71d877dddf",
+    "url": "/portfolio/static/media/CV_ElenaOrtegaFernandez_ES.fe9e5e32.pdf"
+  },
+  {
+    "revision": "b166e18dc1d08d26044381030c0662eb",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.f14d44154706edb60366995271ca9328.js"
+  "/portfolio/precache-manifest.471eb32ae4ae32bf26e33d56dbed277a.js"
 );
 
 workbox.clientsClaim();
