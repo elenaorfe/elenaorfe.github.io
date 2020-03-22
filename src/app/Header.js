a11edd236b7a67095cbcb29ddfbd9240c952878c
elenaortega <elenaorfe@gmail.com>
2020-03-22 11:44:46 +0100

    Adjust print styles

--- src/app/Header.js
+++ src/app/Header.js
@@ -7,6 +7,12 @@ class Header extends Component {
             <header className="bg-primary-light text-center">
                 <h1><FormattedMessage id="about.name"/></h1>
                 <h2><FormattedMessage id="about.role"/></h2>
+                <div className="d-none d-print-block separator-vertical">
+                    <a href="tel:+34646700259" target="_blank" className="print-no-link">+34 646700259</a>
+                    <a href="mailto:elenaorfe@gmail.com" target="_blank">elenaorfe@gmail.com</a>
+                    <a href="https://www.linkedin.com/in/elenaorfe" target="_blank">Linkedin</a>
+                    <a href="https://github.com/elenaorfe" target="_blank">Github</a>
+                </div>
             </header>
         );
     }
